0e25be7b75f8b3280bdb2dd8dabc2c138cd19464
Fausto Santos <fst.santos@gmail.com>
2017-04-11 15:46:45 -0300

    Added windows build

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Linux build
 go build -o jira-kanban-metrics -v jira-kanban-metrics.go jira.go jira-board.go parsing.go struct.go
+
+# Windows build
+GOOS=windows GOARCH=386 go build -o jira-kanban-metrics.exe -v jira-kanban-metrics.go jira.go jira-board.go parsing.go struct.go
