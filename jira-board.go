c52584db353a7f92dd14223d29fae0b82df3b999
Fausto Santos <falsantos@uolinc.com>
2017-04-11 11:49:01 -0300

    Refactoring

---
+++ jira-board.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"encoding/json"
+)
+
+func loadBoardCfg() BoardCfg {
+	if _, err := os.Stat("jira-board.cfg"); os.IsNotExist(err) {
+		panic("jira-board.cfg not found")
+	}
+
+	file, _ := os.Open("jira-board.cfg")
+	decoder := json.NewDecoder(file)
+	boardCfg := BoardCfg{}
+	err := decoder.Decode(&boardCfg)
+	
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return boardCfg
+}
